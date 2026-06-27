33612b82879ba024a8f9a2e85ef0e4c175ea1f5b
postclip69-ux <postclip69@gmail.com>
2026-06-28 01:07:31 +0800

    Update stock.js

--- stock.js
+++ stock.js
@@ -49,7 +49,7 @@ export default async function handler(req, res) {
 
         const response =
             await fetch(
-                `${protocol}://${host}/api/proxy`
+                `${protocol}://${host}/stock`
             );
 
         const data =
